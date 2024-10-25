d9f769e44e1c1446004bfa80ccc7e375b60b29b0
286111 <Akash.R@ust.com>
2024-10-25 13:07:19 +0530

    new commit

--- src/main/java/com/ust_cicd_app/api/MyApi.java
+++ src/main/java/com/ust_cicd_app/api/MyApi.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyApi {
     @GetMapping
     public String hello(){
-        return  "Hello new World";
+        return  "Hello  World welcome to ust";
     }
 
 }
