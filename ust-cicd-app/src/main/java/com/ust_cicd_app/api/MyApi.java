449cf57ee71513f66dee474b7c433c127ba19777
286111 <Akash.R@ust.com>
2024-10-25 11:45:10 +0530

    inital commit

---
+++ ust-cicd-app/src/main/java/com/ust_cicd_app/api/MyApi.java
@@ -0,0 +1,12 @@
+package com.ust_cicd_app.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyApi {
+    @GetMapping
+    public String hello(){
+        return  "Hello World";
+    }
+}
---
+++ ust-cicd-app/src/test/java/spring_boot_app/ust_cicd_app/UstCicdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_app.ust_cicd_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UstCicdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
